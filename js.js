f13859fd995c83a3393d2ca0818e4f4c7c47ab52
James Bradford Dankwah <42591648+JamesBradford97@users.noreply.github.com>
2024-05-24 11:29:23 +0800

    Add reset button and prompt

--- js.js
+++ js.js
@@ -1,5 +1,17 @@
 const containerDiv = document.querySelector("#container");
 
+const resetBtn = document.createElement("button");
+
+let sides = 16;
+
+resetBtn.textContent = "Reset Canvas";
+
+resetBtn.addEventListener("click",()=>{
+    sides = prompt("Please enter number of squares per side (<100): ");
+});
+
+containerDiv.appendChild(resetBtn);
+
 for (j=0;j<16;j++){
     const rowDiv = document.createElement("div");
     rowDiv.setAttribute("class","rowDiv");
