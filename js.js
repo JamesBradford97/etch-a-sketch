b17bf64e4de8bcc9982cf9a55f259fba1aca4cf1
James Bradford Dankwah <42591648+JamesBradford97@users.noreply.github.com>
2024-05-24 11:32:49 +0800

    Add printGrid function

--- js.js
+++ js.js
@@ -12,18 +12,21 @@ resetBtn.addEventListener("click",()=>{
 
 containerDiv.appendChild(resetBtn);
 
-for (j=0;j<16;j++){
-    const rowDiv = document.createElement("div");
-    rowDiv.setAttribute("class","rowDiv");
-
-    for (i=0;i<16;i++){
-        const gridDiv = document.createElement("div");
-        gridDiv.setAttribute("class","gridDiv");
-        gridDiv.addEventListener("mouseenter",()=>{
-            gridDiv.setAttribute("class","gridDiv hover");
-        });
-        rowDiv.appendChild(gridDiv);
+function printGrid(sides, containerDiv){
+    for (j=0;j<sides;j++){
+        const rowDiv = document.createElement("div");
+        rowDiv.setAttribute("class","rowDiv");
+    
+        for (i=0;i<sides;i++){
+            const gridDiv = document.createElement("div");
+            gridDiv.setAttribute("class","gridDiv");
+            gridDiv.addEventListener("mouseenter",()=>{
+                gridDiv.setAttribute("class","gridDiv hover");
+            });
+            rowDiv.appendChild(gridDiv);
+        }
+        containerDiv.appendChild(rowDiv);
     }
-    containerDiv.appendChild(rowDiv);
 }
 
+printGrid(sides, containerDiv);
